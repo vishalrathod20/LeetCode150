f906a0f2908d0ccc6d2ce7d9b9376f300b65af1d
Vishal Rathod <vishalrathod202004@gmail.com>
2024-10-10 14:20:39 +0530

    Create 149. Max Points on a Line.cpp

---
+++ 149. Max Points on a Line.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int maxPoints(vector<vector<int>>& points) {
+        int n = points.size();
+
+        if(n==1) return 1;
+
+        int result = 0;
+
+        for(int i = 0; i<n; i++){
+            unordered_map<double,int> map;
+
+            for(int j = 0; j<n; j++){
+                if(j == i) continue;
+
+                auto dy = points[j][1] - points[i][1];
+                auto dx = points[j][0] - points[i][0];
+
+                auto theta = atan2(dy, dx);
+
+                map[theta]++;
+            }
+
+            for(auto &it : map){
+                result = max(result,it.second+1);
+            }
+        }
+        return result;
+    }
+};
